05e1edf5f33778aa8bbdba56fd46b278a17cb96e
simonojok19 <simonojok19@gmail.com>
2021-06-10 19:53:48 +0300

    added navigators

--- App.js
+++ App.js
@@ -3,13 +3,24 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 import Home from './screens/Home';
 import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" hidden />
       <NavigationContainer>
-
+        <Stack.Navigator initialRouteName="Home" headerMode="screen">
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
       </NavigationContainer>
     </>
   );
