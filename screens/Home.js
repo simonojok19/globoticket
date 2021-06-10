ac422b572b364c39862487179c00222274686cee
simonojok19 <simonojok19@gmail.com>
2021-06-10 13:11:46 +0300

    finished the home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,22 +1,49 @@
 import React from 'react';
-import {View, Image, Text} from 'react-native';
+import {View, Image, Text, StyleSheet} from 'react-native';
 
-const introText = 'Quid ad utilitatem tantae pecuniae? Ut nemo dubitet, eorum omnia officia quo spectare, quid sequi, quid fugere debeant? Eodem modo is enim tibi nemo dabit, quod, expetendum sit, id esse laudabile. Qui-vere falsone, quaerere mittimus-dicitur oculis se privasse; Dolor ergo, id est summum malum, metuetur semper, etiamsi non aderit; Videmusne ut pueri ne verberibus quidem a contemplandis rebus perquirendisque deterreantur?';
+const introText =
+  'Quid ad utilitatem tantae pecuniae? Ut nemo dubitet, eorum omnia officia quo spectare, quid sequi, quid fugere debeant? Eodem modo is enim tibi nemo dabit, quod, expetendum sit, id esse laudabile. Qui-vere falsone, quaerere mittimus-dicitur oculis se privasse; Dolor ergo, id est summum malum, metuetur semper, etiamsi non aderit; Videmusne ut pueri ne verberibus quidem a contemplandis rebus perquirendisque deterreantur?';
 
 const Home = props => {
   return (
-    <View style={{flexDirection: 'column'}}>
+    <View style={styles.container}>
       <Image
         source={require('./images/_Export_globoticket-bug-black.png')}
         style={styles.globologo}
       />
       <Text style={styles.title}>Welcome TO GloboTicket</Text>
-      <Text style={style.subtitle}>{props.username}</Text>
+      <Text style={styles.subtitle}>{props.username}</Text>
       <View style={styles.textcontainer}>
         <Text style={styles.content}>{introText}</Text>
       </View>
   </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 20,
+  },
+  textcontainer: {
+    textAlign: 'center',
+    paddingTop: 10,
+  },
+  globologo: {
+    height: 100,
+    width: 150,
+  },
+  title: {
+    fontFamily: 'Ubuntu-Regular',
+  },
+  subtitle: {
+    paddingTop: 5,
+    fontFamily: 'Ubuntu-Regular',
+  },
+  content: {
+    fontFamily: 'Ubuntu-Light',
+    fontWeight: '300',
+  },
+});
 
 export default Home;
