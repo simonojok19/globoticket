08a2d48c33cd263e871f0023bd3f023f0461a458
simonojok19 <simonojok19@gmail.com>
2021-06-10 23:35:50 +0300

    added the news component

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Tickets from './Tickets';
 import Contact from './Contact';
 import TicketPurchase from './TicketPurchase';
+import News from './News';
 
 const Stack = createStackNavigator();
 
@@ -51,6 +52,15 @@ const App = () => {
                 fontFamily: 'Ubuntu-Regular',
               },
               headerTitle: 'Purchase Tickets',
+            }}
+          />
+          <Stack.Screen
+            name="News"
+            component={News}
+            options={{
+              headerTitleAlign: 'center',
+              headerTitleStyle: {fontFamily: 'Ubuntu-Regular'},
+              headerTitle: 'Latest News',
             }}
           />
         </Stack.Navigator>
