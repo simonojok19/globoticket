b8b2d85e12e0256fca355f6ba33ff0150d64ec8f
simonojok19 <simonojok19@gmail.com>
2021-06-10 20:53:56 +0300

    finished the module

--- Home.js
+++ Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, Image, Text, StyleSheet} from 'react-native';
+import Menu from "./Menu";
 
 const introText =
   'Quid ad utilitatem tantae pecuniae? Ut nemo dubitet, eorum omnia officia quo spectare, quid sequi, quid fugere debeant? Eodem modo is enim tibi nemo dabit, quod, expetendum sit, id esse laudabile. Qui-vere falsone, quaerere mittimus-dicitur oculis se privasse; Dolor ergo, id est summum malum, metuetur semper, etiamsi non aderit; Videmusne ut pueri ne verberibus quidem a contemplandis rebus perquirendisque deterreantur?';
@@ -13,8 +14,12 @@ const Home = props => {
       />
       <Text style={styles.title}>Welcome TO GloboTicket</Text>
       <Text style={styles.subtitle}>{props.username}</Text>
+      <Image style={styles.heroimage} source={require('./images/boxing.jpg')} />
       <View style={styles.textcontainer}>
         <Text style={styles.content}>{introText}</Text>
+      </View>
+      <View style={styles.menu}>
+        <Menu />
       </View>
     </View>
   );
@@ -23,11 +28,12 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
     alignItems: 'center',
-    padding: 20,
+    paddingTop: 20,
+    paddingBottom: 20,
+    flex: 1,
   },
   textcontainer: {
-    textAlign: 'center',
-    paddingTop: 10,
+    padding: 20,
   },
   globologo: {
     height: 100,
@@ -43,6 +49,14 @@ const styles = StyleSheet.create({
   content: {
     fontFamily: 'Ubuntu-Light',
     fontWeight: '300',
+  },
+  heroimage: {
+    height: 170,
+    width: '100%',
+  },
+  menu: {
+    position: 'absolute',
+    bottom: 10,
   },
 });
 
