492068e31bc2367967902767ea77292f2ecd0d56
simonojok19 <simonojok19@gmail.com>
2021-06-10 13:03:43 +0300

    added fonts

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {View, Image, Text} from 'react-native';
 
-let introText = 'Welcome here';
+const introText = 'Quid ad utilitatem tantae pecuniae? Ut nemo dubitet, eorum omnia officia quo spectare, quid sequi, quid fugere debeant? Eodem modo is enim tibi nemo dabit, quod, expetendum sit, id esse laudabile. Qui-vere falsone, quaerere mittimus-dicitur oculis se privasse; Dolor ergo, id est summum malum, metuetur semper, etiamsi non aderit; Videmusne ut pueri ne verberibus quidem a contemplandis rebus perquirendisque deterreantur?';
 
 const Home = props => {
   return (
     <View style={{flexDirection: 'column'}}>
       <Image
-        source={require('./images/_Export_globoticket-')}
+        source={require('./images/_Export_globoticket-bug-black.png')}
         style={styles.globologo}
       />
       <Text style={styles.title}>Welcome TO GloboTicket</Text>
