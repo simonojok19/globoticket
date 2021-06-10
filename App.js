151c85dc774a804ff52ed28e0eda0d345c53ba3c
simonojok19 <simonojok19@gmail.com>
2021-06-10 19:49:16 +0300

    added navigation container

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {StatusBar} from 'react-native';
 import Home from './screens/Home';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" hidden />
-      <Home username="Sports Fan" />
+      <NavigationContainer>
+
+      </NavigationContainer>
     </>
   );
 };
