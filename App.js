3737e1191870d5cd9066047130ccb4aba1610e46
simonojok19 <simonojok19@gmail.com>
2021-06-10 13:18:11 +0300

    cleaned the ui and app.js components

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
+import Home from './screens/Home';
 
-const App: () => React$Node = () => {
+const App = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
+      <StatusBar barStyle="dark-content" hidden />
+      <Home username="Sports Fan" />
     </>
   );
 };
